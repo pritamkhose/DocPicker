161c177175429c52798f359291815d47fe8d704d
Pritam Khose <pritamkhose@gmail.com>
2021-12-01 20:29:16 +0530

    update report

--- __tests__/components/Intro.test.js
+++ __tests__/components/Intro.test.js
@@ -3,10 +3,10 @@ import renderer from 'react-test-renderer';
 import Intro from '../../components/Intro';
 import { FlatList, Text } from 'react-native';
 
-// test('renders correctly', () => {
-//   const tree = renderer.create(<Intro />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+test('renders correctly', () => {
+  const tree = renderer.create(<Intro />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
 it('renders the Flatlist component', () => {
   const tree = renderer.create(
